b4376dcf053313693f515b0e7ee1d3d2f164de80
The Cloud Forest Retreat <thecloudforestretreat@gmail.com>
2025-12-18 20:49:15 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,9 @@
+(function () {
+  const path = (location.pathname || "/").replace(/\/+$/, "") || "/";
+  const links = document.querySelectorAll('[data-nav] a');
 
+  links.forEach(a => {
+    const href = (a.getAttribute("href") || "").replace(/\/+$/, "") || "/";
+    if (href === path) a.classList.add("active");
+  });
+})();
